c9f7527eee0c45983b14c38520674ee04e997113
d-aggarwal <drishtiaggarwal6284@gmail.com>
2025-05-27 11:40:44 +0530

    Add relationships for CloudTrailTrail and CloudWatch
    Signed-off-by: d-aggarwal <drishtiaggarwal6284@gmail.com>

--- tests/data/aws/cloudtrail.py
+++ tests/data/aws/cloudtrail.py
@@ -1,24 +1,25 @@
 LIST_CLOUDTRAIL_TRAILS = [
     {
-        "TrailARN": "arn:aws:cloudtrail:us-east-1:123456789012:trail/test-trail",
+        "TrailARN": "arn:aws:cloudtrail:eu-west-1:000000000000:trail/test-trail",
         "Name": "test-trail",
-        "HomeRegion": "us-east-1",
+        "HomeRegion": "eu-west-1",
     }
 ]
 
 GET_CLOUDTRAIL_TRAIL = {
     "Name": "test-trail",
-    "HomeRegion": "us-east-1",
-    "TrailARN": "arn:aws:cloudtrail:us-east-1:123456789012:trail/test-trail",
+    "HomeRegion": "eu-west-1",
+    "TrailARN": "arn:aws:cloudtrail:eu-west-1:000000000000:trail/test-trail",
     "IsMultiRegionTrail": True,
     "IsOrganizationTrail": False,
     "LogFileValidationEnabled": True,
     "S3BucketName": "test-bucket",
     "S3KeyPrefix": "test-prefix",
+    "SnsTopicARN": "arn:aws:sns:eu-west-1:000000000000:test-topic",
     "IncludeGlobalServiceEvents": True,
     "IsMultiRegionTrail": True,
     "HasCustomEventSelectors": False,
     "HasInsightSelectors": False,
-    "KmsKeyId": "arn:aws:kms:us-east-1:123456789012:key/test-key",
-    "CloudWatchLogsLogGroupArn": "arn:aws:logs:us-east-1:123456789012:log-group:test-log-group",
+    "KmsKeyId": "9a1ad414-6e3b-47ce-8366-6b8f26ba467d",
+    "CloudWatchLogsLogGroupArn": "arn:aws:logs:eu-west-1:000000000000:log-group:/aws/codebuild/sample-project",
 }
